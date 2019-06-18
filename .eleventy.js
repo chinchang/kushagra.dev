9b589b5d4185db8f004de02d230a97d2024804a2
Kushagra Gour <chinchang457@gmail.com>
2019-06-18 23:01:12 +0530

    more finishing

--- .eleventy.js
+++ .eleventy.js
@@ -11,7 +11,7 @@ const markdownItConfig = {
 const markdownItAnchorConfig = {
   permalink: true,
   permalinkClass: "bookmark",
-  permalinkSymbol: "ℙ"
+  permalinkSymbol: "#"
 };
 
 const markdownLib = markdownIt(markdownItConfig).use(
